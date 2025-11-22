9e83c30c357766f631fcb9682d79788c30d9b908
Sheylarp <rivera.sheyla.p@gmail.com>
2025-11-21 19:27:40 -0600

    delete direccion

--- src/main/java/com/digis01/ECarvajalProgramacionEnCapasOctubre2025/Controller/UsuarioController.java
+++ src/main/java/com/digis01/ECarvajalProgramacionEnCapasOctubre2025/Controller/UsuarioController.java
@@ -2,26 +2,42 @@ package com.digis01.ECarvajalProgramacionEnCapasOctubre2025.Controller;
 
 
 import com.digis01.ECarvajalProgramacionEnCapasOctubre2025.ML.Direccion;
+import com.digis01.ECarvajalProgramacionEnCapasOctubre2025.ML.Estado;
 import com.digis01.ECarvajalProgramacionEnCapasOctubre2025.ML.Pais;
 import com.digis01.ECarvajalProgramacionEnCapasOctubre2025.ML.Result;
 import com.digis01.ECarvajalProgramacionEnCapasOctubre2025.ML.Roll;
 import com.digis01.ECarvajalProgramacionEnCapasOctubre2025.ML.Usuario;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.validation.Valid;
+import java.io.IOException;
 import java.lang.ProcessBuilder.Redirect.Type;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.modelmapper.TypeToken;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import static org.springframework.http.converter.json.Jackson2ObjectMapperBuilder.json;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
@@ -45,28 +61,19 @@ public class UsuarioController {
                 HttpEntity.EMPTY,
                 new ParameterizedTypeReference<Result<Usuario>>() {}
             );
-        ResponseEntity<Result<Roll>> responseEntityRoll =
-            restTemplate.exchange(
-                url + "/roll",
-                HttpMethod.GET,
-                HttpEntity.EMPTY,
-                new ParameterizedTypeReference<Result<Roll>>() {}
-            );
+        
 
-        String error = "error usuario : " + responseEntityUsuario.getBody().errorMessage + "error roll : " + responseEntityRoll.getBody();
+        String error = "error usuario : " + responseEntityUsuario.getBody().errorMessage;
 //        String error = responseEntityUsuario.getBody().errorMessage;
-        if(responseEntityUsuario.getStatusCode().value() == 200 && responseEntityRoll.getStatusCode().value() == 200) {
+        if(responseEntityUsuario.getStatusCode().value() == 200) {
             
             
             
             Result resultUsuario = responseEntityUsuario.getBody();
-            Result resultRoll = responseEntityRoll.getBody();
             
             
             
             model.addAttribute("usuarios", resultUsuario.objects);
-            model.addAttribute("rolles", resultRoll.objects);
-      
          }
             else {
             
@@ -80,18 +87,19 @@ public class UsuarioController {
 
     }
     
+    
     @GetMapping("/detail/{idUsuario}")
     public String GetById(@PathVariable("idUsuario") int idUsuario, Model model) {
         
         RestTemplate restTemplate = new RestTemplate();
         
         String id = Integer.toString(idUsuario);
-        ResponseEntity<Result<List<Usuario>>> responseEntityUsuario =
+        ResponseEntity<Result<Usuario>> responseEntityUsuario =
             restTemplate.exchange(
                 url + "/usuario/" + id,
                 HttpMethod.GET,
                 HttpEntity.EMPTY,
-                new ParameterizedTypeReference<Result<List<Usuario>>>() {}
+                new ParameterizedTypeReference<Result<Usuario>>() {}
             );
          ResponseEntity<Result<Roll>> responseEntityRoll =
             restTemplate.exchange(
@@ -108,7 +116,9 @@ public class UsuarioController {
                 new ParameterizedTypeReference<Result<Pais>>() {}
             );
         
-        if (responseEntityUsuario.getStatusCode().value() == 200) {
+        if (responseEntityUsuario.getStatusCode().value() == 200 && 
+               responseEntityRoll.getStatusCode().value() == 200 && 
+               responseEntityPais.getStatusCode().value() == 200) {
             
             Result resultUsuario = responseEntityUsuario.getBody();
             Result resultRoll = responseEntityRoll.getBody();
@@ -129,5 +139,159 @@ public class UsuarioController {
 
     }
     
+    @GetMapping("delete/{id}")
+    public String Delete(@PathVariable("id") int id, Model model, RedirectAttributes redirectAttributes) {
 
+        RestTemplate restTemplate = new RestTemplate();
+        
+        ResponseEntity<Result<Usuario>> responseEntityUsuario =
+            restTemplate.exchange(
+                url + "/usuario/" + id,
+                HttpMethod.DELETE,
+                HttpEntity.EMPTY,
+                new ParameterizedTypeReference<Result<Usuario>>() {}
+            );
+        
+        if (responseEntityUsuario.getStatusCode().value() == 204) {
+            
+            Result resultUsuario = responseEntityUsuario.getBody();
+           
+            redirectAttributes.addFlashAttribute("successDeleteMessage", "usuario eliminado");
+            
+            
+        } else {
+
+            return "redirect:/usuario?error=Usuario no encontrado";
+
+        }
+        
+        
+        return "redirect:/usuario";
+    }
+    
+    @GetMapping("add")
+    public String Add(Model model) {
+        
+        RestTemplate restTemplate = new RestTemplate();
+        
+         ResponseEntity<Result<Roll>> responseEntityRoll =
+            restTemplate.exchange(
+                url + "/roll",
+                HttpMethod.GET,
+                HttpEntity.EMPTY,
+                new ParameterizedTypeReference<Result<Roll>>() {}
+            );
+         ResponseEntity<Result<Pais>> responseEntityPais =
+            restTemplate.exchange(
+                url + "/pais",
+                HttpMethod.GET,
+                HttpEntity.EMPTY,
+                new ParameterizedTypeReference<Result<Pais>>() {}
+            );
+        
+        Result resultRoll = responseEntityRoll.getBody();
+        Result resultPais = responseEntityPais.getBody();
+         
+        model.addAttribute("rolles", resultRoll.objects);
+        model.addAttribute("paises", resultPais.objects);
+        
+        Usuario usuario = new Usuario();
+        model.addAttribute("Usuario", usuario);
+        
+        model.addAttribute("direccion", new Direccion());
+        return "UsuarioForm";
+    }
+    
+    
+    
+    @PostMapping("add")
+    public String Add(@Valid @ModelAttribute("Usuario") Usuario usuario,
+            BindingResult bindingResult,
+            Model model, RedirectAttributes redirectAttributes,
+            @RequestParam("imagenFile") MultipartFile imagenFile) {
+
+
+        if (imagenFile != null) {
+
+            try {
+
+                //vuelvo a asegurarme que es jpg o png
+                String extension = imagenFile.getOriginalFilename().split("\\.")[1];
+
+                if (extension.equals("jpg") || extension.equals("png")) {
+
+                    byte[] byteImagen = imagenFile.getBytes();
+
+                    String imagenBase64 = Base64.getEncoder().encodeToString(byteImagen);
+
+                    usuario.setImagen(imagenBase64);
+
+                }
+
+            } catch (IOException ex) {
+
+                Logger.getLogger(UsuarioController.class.getName()).log(Level.SEVERE, null, ex);
+
+            }
+
+        }
+        
+        if (!bindingResult.hasErrors()) {
+            
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON); // O el tipo que necesites
+            HttpEntity<Usuario> requestEntity = new HttpEntity<>(usuario, headers);
+            
+            RestTemplate restTemplate = new RestTemplate();
+
+            ResponseEntity<Result<Usuario>> responseEntityUsuario =
+               restTemplate.exchange(
+                   url + "/usuario",
+                   HttpMethod.POST,
+                   requestEntity,
+                   new ParameterizedTypeReference<Result<Usuario>>() {}
+               );
+            
+            Result result = responseEntityUsuario.getBody();
+
+            if (result.correct) {
+
+                redirectAttributes.addFlashAttribute("successMessage", "El usuario " + usuario.getUserName() + "se creo con exito.");
+            } else {
+
+                redirectAttributes.addFlashAttribute("successMessage", "El usuario " + usuario.getUserName() + "no se creo");
+            }
+            
+           
+        }
+        return "redirect:/usuario";
+
+    }
+    
+    @GetMapping("{idUsuario}/deleteDireccion/{idDireccion}")
+    public String DeleteDireccion(@PathVariable("idDireccion") int idDireccion,
+            @PathVariable("idUsuario") int idUsuario,
+            Model model, RedirectAttributes redirectAttributes) {
+
+        
+        RestTemplate restTemplate = new RestTemplate();
+        
+        ResponseEntity<Result> responseEntityDireccion =
+            restTemplate.exchange(
+                url + "/direccion/" + idDireccion,
+                HttpMethod.DELETE,
+                HttpEntity.EMPTY,
+                new ParameterizedTypeReference<Result>() {}
+            );
+        Result resultDireccion = responseEntityDireccion.getBody();
+        
+        
+        if(resultDireccion.correct == true){
+            
+            redirectAttributes.addFlashAttribute("successDeleteDireccionMessage", "Direccion eliminada");
+
+        } 
+       
+        return "redirect:/usuario/detail/" + idUsuario;
+    }
 }
