fb46e81069404af6866577ed54e6131e8bdcc2eb
Sheylarp <rivera.sheyla.p@gmail.com>
2025-12-05 18:30:39 -0600

    token login pt5 incompleto

--- src/main/java/com/digis01/ECarvajalProgramacionEnCapasOctubre2025/Configuration/ClientConfig.java
+++ src/main/java/com/digis01/ECarvajalProgramacionEnCapasOctubre2025/Configuration/ClientConfig.java
@@ -6,9 +6,18 @@ package com.digis01.ECarvajalProgramacionEnCapasOctubre2025.Configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@EnableWebSecurity
 public class ClientConfig {
     
     @Bean
@@ -16,5 +25,32 @@ public class ClientConfig {
         return new RestTemplate();
     }
     
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/css/**", "/js/**", "/img/**").permitAll()
+                        .requestMatchers("/usuario/**").authenticated()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/auth/login")  
+                        .loginProcessingUrl("/auth/logout") 
+                        .failureUrl("/auth/login?error=true")
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/auth/logout")
+                        .logoutSuccessUrl("/auth/login?logout=true")
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                );
+        
+        return http.build();
+
+    }
     
 }
